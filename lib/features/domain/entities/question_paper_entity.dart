7d15c1bdfdc935161a93275ef123ba524691abba
Chinmay01234 <chinmayjavalagi@gmail.com>
2022-12-12 21:12:44 +0530

    Entity and Repository of Domain Layer

--- lib/features/domain/entities/question_paper_entity.dart
+++ lib/features/domain/entities/question_paper_entity.dart
@@ -1,1 +1,24 @@
 // TODO: Add entities for a question paper with attributes
+
+class QuestionPaperEntity {
+  final int? id;
+  final String? departmentName;
+  final String? semester;
+  final String? subjectName;
+  final String? examName;
+  final String? fileURL;
+  final int? year;
+  final String? uploaderName;
+  final String? uploadDate;
+
+  QuestionPaperEntity(
+      {this.id,
+        this.departmentName,
+      this.semester,
+      this.subjectName,
+      this.examName,
+      this.fileURL,
+        this.year,
+      this.uploaderName,
+      this.uploadDate});
+}
